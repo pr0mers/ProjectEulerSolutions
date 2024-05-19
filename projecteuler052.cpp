4e975fe9bf7690cbbd81aacc302199b1136042e8
pr0mers <86203241+pr0mers@users.noreply.github.com>
2024-05-19 23:58:47 +0300

    Add files via upload

---
+++ projecteuler052.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+vector<string>v;
+int dene(int n){
+	string x=to_string(n);
+	sort(x.begin(),x.end());
+	for(int i=2;i<=6;i++){
+		string y=to_string(i*n);
+		sort(y.begin(),y.end());
+		if(y!=x){
+			return 0;
+		}
+	}
+	return 1;
+	
+}
+
+int main(){
+	for(int i=1;;i++){
+		if(dene(i)==1){
+			cout<<i;
+			return 0;
+		}
+	}
+}
